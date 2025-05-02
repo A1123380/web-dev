0ea5d88d7edf769752d7dac9c9075b0096c307a5
A1123380 <A1123380@mail.nuk.edu.tw>
2025-05-02 10:53:08 +0800

    Update sendmail.php

--- class/class6/sendmail.php
+++ class/class6/sendmail.php
@@ -24,8 +24,8 @@ try {
     $mail->isSMTP();                                            //Send using SMTP
     $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-    $mail->Username   = 'monkey60404@gmail.com';                     //SMTP username
-    $mail->Password   = 'gahd frjf clff zxma';                               //SMTP password
+    $mail->Username   = '';                     //SMTP username
+    $mail->Password   = '';                               //SMTP password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
     $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
     $mail->charset    = 'utf-8';
